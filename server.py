64ae972327ebf0c13567876594d96c8e6319f003
Sophia K <kholod@ucu.edu.ua>
2019-06-05 00:11:17 +0300

    minor changes to server.py

--- server.py
+++ server.py
@@ -1,12 +1,15 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, jsonify
 from flask_login import login_required, current_user
 from flask_login import LoginManager
-from models import Client, ClientLog, app, db
+from models import Clients, ClientLog, app, db
+from clients_controller import app_db
 
 main = Blueprint('main', __name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///vaccina.db'
+app.config['JSON_AS_ASCII'] = False
+app.register_blueprint(app_db)
 db.init_app(app)
 login_manager = LoginManager()
 login_manager.login_view = 'auth.login'
@@ -32,20 +35,19 @@ def home():
 @main.route('/profile')
 @login_required
 def profile():
-    cli = Client.query.filter_by(id=current_user.id).first()
+    cli = Clients.query.filter_by(id=current_user.id).first()
     return render_template('profile.html', client=cli)
-
 
 
 @main.route('/profile', methods=['POST'])
 def get_profile():
-    cli = Client.query.filter_by(id=current_user.id).first()
+    cli = Clients.query.filter_by(id=current_user.id).first()
 
     if cli is None:
-        cli = Client(id=current_user.id, first_name=request.form.get('firstname'),
-                     last_name=request.form.get('lastname'),
-                     sex=request.form.get('sex'),
-                     date_of_birth=request.form.get('date'), location=request.form.get('location'))
+        cli = Clients(id=current_user.id, first_name=request.form.get('firstname'),
+                      last_name=request.form.get('lastname'),
+                      sex=request.form.get('sex'),
+                      date_of_birth=request.form.get('date'), location=request.form.get('location'))
         db.session.add(cli)
     else:
         cli.first_name = request.form.get('firstname')
@@ -55,6 +57,12 @@ def get_profile():
         cli.location = request.form.get('location')
     db.session.commit()
     return redirect(url_for('main.home'))
+
+
+@login_required
+@blueprint.route("/profile/current_user")
+def get_current_user():
+    return jsonify({"id": current_user.id})
 
 
 @blueprint.route("/search")
@@ -98,7 +106,6 @@ from auth import auth as auth_blueprint
 app.register_blueprint(auth_blueprint)
 app.register_blueprint(main)
 app.register_blueprint(blueprint, url_prefix="/")
-
 
 if __name__ == '__main__':
     db.create_all()
